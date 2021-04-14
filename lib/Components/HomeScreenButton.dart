fc2b55105f0aa2c65e743ae16f64ec6b759125d5
Thomas Ewalts <thomas.ewalts@gmail.com>
2021-04-14 14:43:56 +0200

    Explanationscreen done, changed a few other minor things

--- lib/Components/HomeScreenButton.dart
+++ lib/Components/HomeScreenButton.dart
@@ -1,8 +1,8 @@
+import 'package:code_cadette/Pages/ExplanationScreen.dart';
 import 'package:flutter/material.dart';
 import 'package:code_cadette/Components/ColorClass.dart';
 
-
-//Deze klasse bevat de homescreen buttons voor de verschillende leerdoelen
+//Deze bevat de homescreen button voor de verschillende leerdoelen
 class HomeScreenButton extends StatelessWidget {
   final String title;
   final Widget destination; //linkt de juiste page
@@ -11,14 +11,20 @@ class HomeScreenButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return InkWell(
-        onTap: () => debugPrint('testtrigger'),
+        onTap: () {
+          Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => this.destination,
+              ));
+        },
         child: Container(
           padding: EdgeInsets.only(top: 10.0, bottom: 10.0),
           child: SizedBox(
-            height: 200,
+            height: MediaQuery.of(context).size.height * 0.2,
             child: DecoratedBox(
               decoration: BoxDecoration(
-                  color: ColorClass.mainScreenButtonBackground,
+                  color: ColorClass.mainScreenButtonsecondary,
                   borderRadius: BorderRadius.all(Radius.circular(10.0))),
               child: Center(
                   child: Text(
